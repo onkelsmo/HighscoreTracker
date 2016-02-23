a174aa209aebb48c7250cb3289d4613d963775d3
Jan <onkelsmo@gmail.com>
2016-02-23 12:18:33 +0100

    MOD: ActionBar for GenericGameActivity

--- app/src/main/java/com/onkelsmo/highscoretracker/MainActivity.java
+++ app/src/main/java/com/onkelsmo/highscoretracker/MainActivity.java
@@ -5,27 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.onkelsmo.highscoretracker.gameActivities.GenericGameActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner gameSelector;
+    private String[] gameStringArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // TODO: 14.02.2016 get games from database?
+        gameStringArray = new String[]{
+                getString(R.string.generic_game),
+                getString(R.string.robinson_crusoe),
+        };
+
+        Button newScoreButton = (Button) findViewById(R.id.new_score_button);
+        newScoreButton.setOnClickListener(this);
+
         gameSelector = (Spinner)findViewById(R.id.game_selector);
         ArrayAdapter<String> gameSelectorAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_spinner_item,
-                // TODO: 14.02.2016 get games from database?
-                new String[]{
-                        getString(R.string.generic_game),
-                        getString(R.string.robinson_crusoe),
-                }
+                gameStringArray
         );
         gameSelectorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gameSelector.setAdapter(gameSelectorAdapter);
@@ -34,9 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.new_score_button) {
-            int selectedGame = gameSelector.getSelectedItemPosition();
+            String selectedGame = (String)gameSelector.getSelectedItem();
             // TODO: 22.02.2016 the new activity has to be specific by the chosen game... how do we solve this?
-            if (selectedGame == R.string.generic_game) {
+            if (selectedGame.equals(gameStringArray[0])) {
                 Intent intent = new Intent(this, GenericGameActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/onkelsmo/highscoretracker/gameActivities/GenericGameActivity.java
+++ app/src/main/java/com/onkelsmo/highscoretracker/gameActivities/GenericGameActivity.java
@@ -1,7 +1,9 @@
 package com.onkelsmo.highscoretracker.gameActivities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.onkelsmo.highscoretracker.R;
 
@@ -11,5 +13,18 @@ public class GenericGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generic_game);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.generic_game);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
+
+
 }
